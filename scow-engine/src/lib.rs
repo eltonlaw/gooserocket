ae32765d20d9130d79ee442075ba7f8fa43502b6
Elton Law <eltonlaw296@gmail.com>
2023-04-17 22:38:21 -0400

    feat: deploy ec2 with cloudformation

--- scow-engine/src/lib.rs
+++ scow-engine/src/lib.rs
@@ -3,5 +3,13 @@ use tokio::runtime::Runtime;
 
 pub fn deploy_jupyter_notebook() {
     let rt = Runtime::new().unwrap();
-    rt.block_on(cloudformation::create_stack());
+    let result = rt.block_on(cloudformation::create_stack(
+            String::from("scow-jupyter-nb-1"),
+            String::from("./assets/jupyter_notebook.yaml")
+        ));
+
+    match result {
+        Ok(_) => println!("Cloudformation create stack worked successfully"),
+        Err(error) => eprintln!("Error: {}", error),
+    }
 }
--- scow-infra/src/aws/cloudformation.rs
+++ scow-infra/src/aws/cloudformation.rs
@@ -1,10 +1,22 @@
+use std::error;
+use std::fs;
 use aws_config;
 use aws_sdk_cloudformation as cf;
 
-pub async fn create_stack() -> Result<(), cloudformation::Error> {
-    println!("Creating new stack");
+pub async fn create_stack(stack_name: String, stack_fp: String)
+    -> Result<(), Box<dyn error::Error>> {
+    let template_body = fs::read_to_string(&stack_fp)?;
+    println!("Creating new stack {} from fp {} with body\n{}", stack_name, stack_fp, template_body);
+
     let config = aws_config::from_env().load().await;
     let client = cf::Client::new(&config);
+
+    client
+        .create_stack()
+        .stack_name(stack_name)
+        .template_body(template_body)
+        .send()
+        .await?;
 
     Ok(())
 }
