17587d1a1f56892a226c1657ec768ef7089ada55
Elton Law <eltonlaw296@gmail.com>
2023-04-18 01:12:37 -0400

    fix: pathing to yaml file doesnt work when not in root project

--- scow-engine/src/lib.rs
+++ scow-engine/src/lib.rs
@@ -5,7 +5,7 @@ pub fn deploy_jupyter_notebook() {
     let rt = Runtime::new().unwrap();
     let result = rt.block_on(cloudformation::create_stack(
             String::from("scow-jupyter-nb-1"),
-            String::from("./assets/jupyter_notebook.yaml")
+            String::from("../assets/jupyter_notebook.yaml"),
         ));
 
     match result {
--- scow-infra/src/aws/cloudformation.rs
+++ scow-infra/src/aws/cloudformation.rs
@@ -1,12 +1,16 @@
 use std::error;
+use std::env;
 use std::fs;
+use std::path::PathBuf;
 use aws_config;
 use aws_sdk_cloudformation as cf;
 
 pub async fn create_stack(stack_name: String, stack_fp: String)
     -> Result<(), Box<dyn error::Error>> {
-    let template_body = fs::read_to_string(&stack_fp)?;
-    println!("Creating new stack {} from fp {} with body\n{}", stack_name, stack_fp, template_body);
+    let mut stack_abs_fp = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    stack_abs_fp.push(stack_fp.as_str());
+    let template_body = fs::read_to_string(&stack_abs_fp)?;
+    println!("Creating new stack {} from fp {:?} with body\n{}", stack_name, stack_abs_fp, template_body);
 
     let config = aws_config::from_env().load().await;
     let client = cf::Client::new(&config);
