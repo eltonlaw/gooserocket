666239cf23b7513321275ed3def74c794f87b0b0
Elton Law <eltonlaw296@gmail.com>
2024-04-05 01:52:51 -0400

    update bio

--- nix/python.nix
+++ nix/python.nix
@@ -1,18 +1,17 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 with pkgs;
 rec {
   packages = ps: with ps; [
-    # biopy
     (buildPythonPackage rec {
        pname = "bio";
        version = "1.5.9";
        format = "wheel";
        src = fetchurl {
-         url = "https://files.pythonhosted.org/packages/79/ab/ad10761bdf295a5f2bfdc2734ed61d3c82aa570f0b679428dbe2caa2727c/bio-1.5.9-py3-none-any.whl";
-         sha256 = "sha256-scROUJkilVpdh94hX303N5ayeHQXNdhs5WY5ZdXPOGs=";
+         url = "https://files.pythonhosted.org/packages/c1/1e/6c4808999f33f63ed1327c6463d03f1209b08634183a237d9112370071b4/bio-1.6.2-py3-none-any.whl";
+         sha256 = "dcec8207f3993a7f41bd8205cde235218da3dc008175ae7bb7468276d8dacf71";
        };
        propagatedBuildInputs = [ ]; })
-    biopython
+    # biopython
     ipython
     matplotlib
     pandas
